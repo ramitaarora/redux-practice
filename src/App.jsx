46c7ff54628a472fbcd9b7dd272d41bec6c6b4d2
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-08-26 10:57:34 -0700

    html form setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
 export default function App() {
-  return;
+  return (
+    <div>
+      <header>
+        <h1>Redux Practice: To-Do List</h1>
+      </header>
+      <main>
+      
+        <form>
+          <input type="text" name="to-do" placeholder="To Do..."/>
+          <input type="submit" value="Add"/>
+        </form>
+
+        <div>
+          <ol>
+            
+          </ol>
+        </div>
+      </main>
+    </div>
+  )
 }
