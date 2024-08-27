f714c0e20ad5d9597057a77df6f1fa0bfbfe9972
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-08-27 11:36:52 -0700

    installed redux and toolkit, started store functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { createStore } from 'redux';
+
+const notes = [
+  {
+    text: "Wash dishes",
+    completed: false,
+  },
+  {
+    text: "Vacuum",
+    completed: false,
+  },
+]
+
+const notesReducer = (state = notes, action) => {
+  switch (action.type) {
+    case 'completed':
+      return state.completed === false ? true : false;
+    default:
+      return state;
+  }
+}
+
+export const store = createStore(notesReducer);
+
 export default function App() {
   return (
     <div>
